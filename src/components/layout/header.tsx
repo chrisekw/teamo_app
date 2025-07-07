f62fd12aa6a69623adbb0e88962e1991de71f3e2
Christian Ekw <ekwchristian@gmail.com>
2025-07-07 20:39:56 +0000

    Use collapsible sidebar for desktop view

--- src/components/layout/header.tsx
+++ src/components/layout/header.tsx
@@ -15,12 +15,10 @@ export function Header() {
   return (
     <header className="sticky top-0 z-40 w-full border-b bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60">
       <div className="container flex h-14 max-w-screen-2xl items-center">
-        <div className="mr-4 flex">
-          {isMobile && (
-            <SidebarTrigger className="mr-2 h-8 w-8">
-              <PanelLeft className="h-6 w-6" />
-            </SidebarTrigger>
-          )}
+        <div className="mr-4 flex items-center">
+          <SidebarTrigger className="mr-2 h-8 w-8">
+            <PanelLeft className="h-6 w-6" />
+          </SidebarTrigger>
         </div>
         <div className="flex flex-1 items-center justify-end space-x-1 sm:space-x-2">
           {isMobile && (
